376c1df5bac96bce923925c54b4ae7042f41686e
Marco Stolle <marco.stolle@gmail.com>
2026-06-09 18:33:56 +0200

    changed partition order

--- install-UEFI.sh
+++ install-UEFI.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # Create gpt table
 parted /dev/sda -- mklabel gpt
-# Add root partition
-parted /dev/sda -- mkpart root ext4 512MB -8GB
-# Add swap partition
-parted /dev/sda -- mkpart swap linux-swap -8GB 100%
 # Add boot partition
-parted /dev/sda -- mkpart ESP fat32 1MB 512MB
+parted /dev/sda -- mkpart ESP fat32 1MB 1G
+# Add swap partition
+parted /dev/sda -- mkpart swap linux-swap 1G 9G
+# Add root partition
+parted /dev/sda -- mkpart root ext4 9G 100%
 parted /dev/sda -- set 3 esp on
 
 # Formatting
 # Assign a unique symbolic label to the file system
-mkfs.ext4 -L nixos /dev/sda1
+mkfs.fat -F 32 -n boot /dev/sda1
 mkswap -L swap /dev/sda2
-mkfs.fat -F 32 -n boot /dev/sda3
+mkfs.ext4 -L nixos /dev/sda3
 
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
