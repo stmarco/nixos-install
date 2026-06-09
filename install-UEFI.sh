b618ca3d3f66aeea27704d3e0b90fd9ee3161383
Marco Stolle <marco.stolle@gmail.com>
2026-06-09 18:36:47 +0200

    changed sda to vda

--- install-UEFI.sh
+++ install-UEFI.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 # Create gpt table
-parted /dev/sda -- mklabel gpt
+parted /dev/vda -- mklabel gpt
 # Add boot partition
-parted /dev/sda -- mkpart ESP fat32 1MB 1G
+parted /dev/vda -- mkpart ESP fat32 1MB 1G
 # Add swap partition
-parted /dev/sda -- mkpart swap linux-swap 1G 9G
+parted /dev/vda -- mkpart swap linux-swap 1G 9G
 # Add root partition
-parted /dev/sda -- mkpart root ext4 9G 100%
-parted /dev/sda -- set 3 esp on
+parted /dev/vda -- mkpart root ext4 9G 100%
+parted /dev/vda -- set 3 esp on
 
 # Formatting
 # Assign a unique symbolic label to the file system
-mkfs.fat -F 32 -n boot /dev/sda1
-mkswap -L swap /dev/sda2
-mkfs.ext4 -L nixos /dev/sda3
+mkfs.fat -F 32 -n boot /dev/vda1
+mkswap -L swap /dev/vda2
+mkfs.ext4 -L nixos /dev/vda3
 
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
 # turn swap on
-swapon /dev/sda2
+swapon /dev/vda2
 # Generate configuration file
 nixos-generate-config --root /mnt
